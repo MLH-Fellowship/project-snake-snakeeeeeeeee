aadc3285927cf171e7ed5a65cf3b6934c8015842
David Lazaro Fernandez <davidlazaro20@hotmail.com>
2022-06-04 21:03:26 -0500

    Hero & Navbar completed

--- app/app.py
+++ app/app.py
@@ -8,4 +8,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="Mis cojones", url=os.getenv("URL"))
