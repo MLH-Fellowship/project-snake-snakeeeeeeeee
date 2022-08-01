8ffced63f6ba84b7b59eba3396618ae0ee04ceb0
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2022-08-01 15:59:01 -0500

    Delete curl-test.sh

--- curl-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl --request POST http://localhost:5000/api/timeline_post -d 'name=David&email=davidlazaro20@hotmail.com&content=test{"content":"Test"}
-curl http://localhost:5000/api/timeline_post
